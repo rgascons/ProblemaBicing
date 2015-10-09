de272e19a2761bea40b0ec768e232a6e4916fe9b
Anselmo <falc@null.net>
2015-10-09 11:57:15 +0200

    Added GoalTest

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
         Estaciones estaciones = new Estaciones(20, 60, 0, 1234);
         System.out.println("Hello World!");
-        //Problem problem = new Problem(estaciones, new FuncionSucesores(), new GoalTest(), new FuncionHeuristica());
+        //Problem problem = new Problem(estaciones, new FuncionSucesora(), new GoalTest(), new FuncionHeuristica());
         Search search = new AStarSearch(new GraphSearch());
         //SearchAgent agent = new SearchAgent(problem, search);
 
