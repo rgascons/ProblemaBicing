21c37a6d357d2931ab16d421ce201c1511240829
jugon09 <camilocastro12@hotmail.comgit config --global user.email camilocastro12@hotmail.comgit config --global user.email camilocastro12@hotmail.com>
2015-10-09 12:03:06 +0200

    LOL

--- src/Estado.java
+++ src/Estado.java
@@ -8,11 +8,17 @@ public class Estado {
     private Estaciones estaciones;
     private ArrayList bicisE;
     private static Random r;
+    public static String[] op = {"sustituir_estacion(nueva_estacion, estacion_antigua, idF)",
+            "dejar_bicis(idF, idE, n)",
+            "recoger_bicis(idF, idE, n)",
+            "cambiar_estacion_origen(idF, idE)",
+            "quitar_estacion(estacion, idF)"
+    };
     
     public Estado(Furgonetas furgonetas, Estaciones estaciones, ArrayList bicisE) {
     }
 
-    public Estado(int nf) {
+    public Estado(int nf, Estaciones estaciones) {
         furgonetas = new Furgonetas(nf, estaciones.size(), System.currentTimeMillis(), estaciones);
         this.estaciones = estaciones;
     }
@@ -38,5 +44,13 @@ public class Estado {
 
     public void setEstaciones(Estaciones estaciones) {
         this.estaciones = estaciones;
+    }
+
+    public boolean puedeSustituirEstacion(int idEVieja, int idENueva, int idF) {
+        return idENueva != idEVieja && (furgonetas.get(idF).getPrimerDestino().equals(estaciones.get(idEVieja)));
+    }
+
+    private boolean dentroLimites(int id) {
+        return 0 >= id && id < estaciones.size();
     }
 }
