6204037e033ebbf22b210c62b6799418b6676952
arkaik <anselmo.lopez.c@gmail.com>
2015-10-12 18:16:46 +0200

    Heurisitic function based on first criteria added (FuncionHeuristicaC1)

---
+++ src/FuncionHeuristicaC1.java
@@ -0,0 +1,28 @@
+/**
+ * Función heurística
+ * Created by falc on 7/10/15.
+ */
+
+import java.util.ArrayList;
+
+import IA.Bicing.Estacion;
+import aima.search.framework.HeuristicFunction;
+
+public class FuncionHeuristicaC1 implements HeuristicFunction{
+    public double getHeuristicValue(Object n)
+    {
+        Estado state = (Estado) n;
+
+        double sum_acord = 0;
+        ArrayList<Estacion> est = state.getEstaciones();
+        for (int i = 0; i < est.size(); ++i)
+        {
+            Estacion e = est.get(i);
+            int bicis_llevadas = state.getBicisE().get(i);
+            int eur = (e.getDemanda() >= e.getNumBicicletasNext()+bicis_llevadas)? bicis_llevadas: -bicis_llevadas;
+            sum_acord += eur;
+        }
+
+        return sum_acord;
+    }
+}
