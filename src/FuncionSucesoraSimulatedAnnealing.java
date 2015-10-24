869bbccefb4ff9a7575218feb0016bb0b49272ef
Ricard Gascons <ricard.gascons@est.fib.upc.edu>
2015-10-24 11:18:52 +0200

    Update FuncionSucesoraSA

--- src/FuncionSucesoraSimulatedAnnealing.java
+++ src/FuncionSucesoraSimulatedAnnealing.java
@@ -13,57 +13,82 @@ public class FuncionSucesoraSimulatedAnnealing implements SuccessorFunction {
         Estado state = (Estado) o;
         Random rand = new Random();
         boolean valid = false;
-        while (!valid)
-        {
+        while (!valid) {
             int n = rand.nextInt(state.getFurgonetas().size());
             Furgoneta f = state.getFurgonetas().get(n);
             int m = rand.nextInt(state.getEstaciones().size());
             Estacion e = state.getEstaciones().get(m);
-            int opt = rand.nextInt(7);
-            switch (opt) {
-                case 0:
-                    if (state.puedeCambiarEstacionOrigen(e, f)) {
-                        Estado nuevoEstado = new Estado(state);
-                        nuevoEstado.cambiarEstacionOrigen(e, f);
-                        Estacion nove = nuevoEstado.getEstaciones().get(m);
-                        retVal.add(new Successor(Estado.CAMBIAR_ESTACION_ORIGEN + " #" + n + "# -> " + Estado.getM().get(nove), nuevoEstado));
-                        valid = true;
-                    }
-                    break;
-                case 1:
-                    if (state.puedeSustituirEstacion(f.getPrimerDestino(), e, f)) {
-                        Estado nuevoEstado = new Estado(state);
-                        nuevoEstado.sustituirEstacion(f.getPrimerDestino(), e, f);
-                        Estacion nove = nuevoEstado.getEstaciones().get(m);
-                        retVal.add(new Successor(Estado.SUSTITUIR_ESTACION + " #" + n + "# " + Estado.getM().get(f.getPrimerDestino()) + " -> " + Estado.getM().get(nove), nuevoEstado));
-                        valid = true;
-                    }
-                    break;
-                case 2:
-                    if (state.puedeSustituirEstacion(f.getSegundoDestino(), e, f)) {
-                        Estado nuevoEstado = new Estado(state);
-                        nuevoEstado.sustituirEstacion(f.getSegundoDestino(), e, f);
-                        Estacion nove = nuevoEstado.getEstaciones().get(m);
-                        retVal.add(new Successor(Estado.SUSTITUIR_ESTACION + " #" + n + "# " + Estado.getM().get(f.getPrimerDestino()) + " -> " + Estado.getM().get(nove), nuevoEstado));
-                        valid = true;
-                    }
-                    break;
-                case 3 :
-                    if (state.puedeQuitarEstacion(e, f))
-                    {
-                        Estado nuevoEstado = new Estado(state);
-                        nuevoEstado.quitarEstacion(e, f);
-                        Estacion nove = nuevoEstado.getEstaciones().get(m);
-                        retVal.add(new Successor(Estado.QUITAR_ESTACION+" #"+n+"# "+Estado.getM().get(nove), nuevoEstado));
-                        valid = true;
-                    }
-                    break;
-                default:
-                    break;
+            int opt = rand.nextInt(280);
+            if (opt < 87) {
+                if (state.puedeCambiarEstacionOrigen(e, f)) {
+                    Estado nuevoEstado = new Estado(state);
+                    nuevoEstado.cambiarEstacionOrigen(e, f);
+                    Estacion nove = nuevoEstado.getEstaciones().get(m);
+                    retVal.add(new Successor(Estado.CAMBIAR_ESTACION_ORIGEN + " #" + n + "# -> " + Estado.getM().get(nove), nuevoEstado));
+                    valid = true;
+                }
+            }
+            else if (opt >= 87 && opt < 174) {
+                if (state.puedeSustituirEstacion(f.getPrimerDestino(), e, f)) {
+                    Estado nuevoEstado = new Estado(state);
+                    nuevoEstado.sustituirEstacion(f.getPrimerDestino(), e, f);
+                    Estacion nove = nuevoEstado.getEstaciones().get(m);
+                    retVal.add(new Successor(Estado.SUSTITUIR_ESTACION + " #" + n + "# " + Estado.getM().get(f.getPrimerDestino()) + " -> " + Estado.getM().get(nove), nuevoEstado));
+                    valid = true;
+                }
+            }
+            else if (opt >= 174 && opt < 261) {
+                if (state.puedeSustituirEstacion(f.getSegundoDestino(), e, f)) {
+                    Estado nuevoEstado = new Estado(state);
+                    nuevoEstado.sustituirEstacion(f.getSegundoDestino(), e, f);
+                    Estacion nove = nuevoEstado.getEstaciones().get(m);
+                    retVal.add(new Successor(Estado.SUSTITUIR_ESTACION + " #" + n + "# " + Estado.getM().get(f.getPrimerDestino()) + " -> " + Estado.getM().get(nove), nuevoEstado));
+                    valid = true;
+                }
+            }
+            else if (opt >= 261 && opt < 264) {
+                if (state.puedeQuitarEstacion(f.getPrimerDestino(), f)) {
+                    Estado nuevoEstado = new Estado(state);
+                    Furgoneta neo = nuevoEstado.getFurgonetas().get(n);
+                    nuevoEstado.quitarEstacion(neo.getPrimerDestino(), f);
+                    Estacion nove = nuevoEstado.getEstaciones().get(m);
+                    retVal.add(new Successor(Estado.QUITAR_ESTACION + " #" + n + "# " + Estado.getM().get(nove), nuevoEstado));
+                    valid = true;
+                }
+            }
+            else if (opt >= 264 && opt < 267) {
+                if (state.puedeQuitarEstacion(f.getSegundoDestino(), f)) {
+                    Estado nuevoEstado = new Estado(state);
+                    Furgoneta neo = nuevoEstado.getFurgonetas().get(n);
+                    nuevoEstado.quitarEstacion(neo.getSegundoDestino(), f);
+                    Estacion nove = nuevoEstado.getEstaciones().get(m);
+                    retVal.add(new Successor(Estado.QUITAR_ESTACION + " #" + n + "# " + Estado.getM().get(nove), nuevoEstado));
+                    valid = true;
+                }
+            }
+            else if (opt >= 267 && opt < 277) {
+                int j = rand.nextInt(f.getBicisEstacionOrigen());
+                if (state.puedeDejarBicis(f, j)) {
+                    Estado nuevoEstado = new Estado(state);
+                    Furgoneta neo = nuevoEstado.getFurgonetas().get(n);
+                    nuevoEstado.dejarBicis(neo, j);
+                    FuncionHeuristicaC1 fhc1 = new FuncionHeuristicaC1();
+                    fhc1.getHeuristicValue(nuevoEstado);
+                    retVal.add(new Successor(Estado.DEJAR_BICIS + " #"+n+"# [[" + j + "]]", nuevoEstado));
+                    valid = true;
+                }
+            }
+            else if (opt >= 277) {
+                if (state.puedeEliminarFurgoneta(n)) {
+                    Estado nuevoEstado = new Estado(state);
+                    nuevoEstado.eliminarFurgoneta(n);
+                    FuncionHeuristicaC1 fhc1 = new FuncionHeuristicaC1();
+                    fhc1.getHeuristicValue(nuevoEstado);
+                    retVal.add(new Successor(Estado.ELIMINAR_FURGONETA+" ~"+n+"~ ", nuevoEstado));
+                    valid = true;
+                }
             }
         }
-        //TODO Revisar número de operadores
-        // TODO
         return retVal;
     }
 }
