851c29e7afb379b94141e84f1f73d35f269e951d
Anselmo <falc@null.net>
2015-10-26 19:23:14 +0100

    Cambios en la heurística

--- src/Dominio/FuncionHeuristica.java
+++ src/Dominio/FuncionHeuristica.java
@@ -27,7 +27,7 @@ public class FuncionHeuristica implements HeuristicFunction{
                 int nb2 = f.getBicisEstacionOrigen() - f.getBicisPrimeraEstacion();
                 int e_k1 = ((nb1 + 9) / 10);
                 int e_k2 = ((nb2 + 9) / 10);
-                sum_cost += e_k1 * (trayecto1/100) + e_k2 * (trayecto2/100);
+                sum_cost += e_k1 * (trayecto1/1000) + e_k2 * (trayecto2/1000);
 
                 distancia += trayecto1+trayecto2;
 
